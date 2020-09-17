e6cfbb630b8818bd0dd40915ec8a5f5471bdc06d
Antonio García <algc1294@gmail.com>
2020-09-17 17:26:23 -0400

    Models reworked. Admin model register.

--- CD/api/admin.py
+++ CD/api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from CD.api.models import (
+    Location,
+    Jail,
+    Town,
+    Player,
+    Slave,
+    CriminalRecord,
+    Section,
+    Page,
+)
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Jail)
+admin.site.register(Town)
+admin.site.register(Player)
+admin.site.register(Slave)
+admin.site.register(CriminalRecord)
+admin.site.register(Section)
+admin.site.register(Page)
--- CD/api/models.py
+++ CD/api/models.py
@@ -1,42 +1,65 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Minecraft Server Models
-
 
 class Location(models.Model):
+    label = models.TextField(max_length=30)
     x = models.FloatField()
     y = models.FloatField()
     z = models.FloatField()
 
+    def __str__(self):
+        return f'{self.label} (x: {self.x}, y: {self.y}, z: {self.z})'
+
 
 class Jail(models.Model):
     name = models.TextField()
-    location_min = models.OneToOneField(Location, on_delete=models.CASCADE, related_name='location_min')
-    location_max = models.OneToOneField(Location, on_delete=models.CASCADE, related_name='location_max')
+    location_min = models.OneToOneField(Location, on_delete=models.CASCADE,
+                                        related_name='location_min')
+    location_max = models.OneToOneField(Location, on_delete=models.CASCADE,
+                                        related_name='location_max')
     is_max_security = models.BooleanField(default=False)
-    slave_spawn = models.OneToOneField(Location, on_delete=models.CASCADE, related_name='slave_spawn')
+    slave_spawn = models.OneToOneField(Location, on_delete=models.CASCADE,
+                                       related_name='slave_spawn')
+
+    def __str__(self):
+        return self.name
 
 
 class Town(models.Model):
     name = models.TextField()
     description = models.TextField()
-    major = models.OneToOneField('Player', on_delete=models.CASCADE, related_name='major')
+    location = models.OneToOneField(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Player(models.Model):
     uuid = models.TextField(primary_key=True)
-    town = models.ForeignKey(Town, on_delete=models.CASCADE, null=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    town = models.ForeignKey(Town, on_delete=models.CASCADE, blank=True,
+                             null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,
+                                null=True)
+    jail = models.ForeignKey(Jail, on_delete=models.CASCADE, blank=True,
+                             null=True)
     is_soldier = models.BooleanField(default=False)
-    jail = models.ForeignKey(Jail, on_delete=models.CASCADE, null=True)
+    is_major = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.uuid
 
 
 class Slave(models.Model):
     slave_timer = models.IntegerField()
     player = models.OneToOneField(Player, on_delete=models.CASCADE)
-    owner = models.ForeignKey(Player, on_delete=models.CASCADE, null=True, related_name='owner')
-    jail = models.ForeignKey(Jail, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True,
+                              null=True, related_name='owner')
+    jail = models.ForeignKey(Jail, on_delete=models.CASCADE, blank=True,
+                             null=True)
+
+    def __str__(self):
+        return self.player.uuid
 
 
 class CriminalRecord(models.Model):
@@ -44,11 +67,15 @@ class CriminalRecord(models.Model):
     message = models.TextField()
     player = models.OneToOneField(Player, on_delete=models.CASCADE)
 
-# Wiki Models
+    def __str__(self):
+        return self.player.uuid
 
 
 class Section(models.Model):
-    name = models.TextField()
+    name = models.TextField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
 class Page(models.Model):
@@ -59,5 +86,11 @@ class Page(models.Model):
     title = models.TextField()
     body = models.TextField()
     type = models.IntegerField(choices=TypeChoices.choices)
-    related_pages = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    related_pages = models.ForeignKey('self', on_delete=models.CASCADE,
+                                      blank=True, null=True)
+    section = models.OneToOneField(Section, on_delete=models.CASCADE)
+    town = models.OneToOneField(Town,  on_delete=models.CASCADE, blank=True,
+                                null=True)
+
+    def __str__(self):
+        return self.title
